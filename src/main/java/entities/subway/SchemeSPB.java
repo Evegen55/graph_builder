808879484edf7bdb7bbc0693bc45d1c091ed63b5
Evgenii Lartcev <i.dolende@gmail.com>
2018-04-15 16:48:45 +0300

    array can be more faster

--- src/main/java/entities/subway/SchemeSPB.java
+++ src/main/java/entities/subway/SchemeSPB.java
@@ -1,12 +1,8 @@
 package entities.subway;
 
-import java.util.Set;
-
 public class SchemeSPB extends Subway {
 
-    private Set<Line> lines;
-
-    public SchemeSPB(Set<Line> lines) {
-        this.lines = lines;
+    public SchemeSPB(Line[] lines) {
+        super.lines = lines;
     }
 }
--- src/main/java/entities/subway/Subway.java
+++ src/main/java/entities/subway/Subway.java
@@ -4,6 +4,6 @@ import java.util.Set;
 
 public abstract class Subway {
 
-    protected Set<Line> lines;
-    protected Set<Interchange> interchanges;
+    protected Line[] lines;
+    protected Interchange[] interchanges;
 }
